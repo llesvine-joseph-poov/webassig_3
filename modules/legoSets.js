41eb36dd2e94f36d132595f1f029f43ffff58dff
llesvine-joseph-poov <126778450+llesvine-joseph-poov@users.noreply.github.com>
2023-12-02 07:22:04 -0500

    Update legoSets.js

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  host: 'ep-square-feather-15731318.us-east-2.aws.neon.tech',
   dialect: 'postgres',
   port: 5432,
   dialectOptions: {
