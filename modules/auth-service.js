1688739377be6eddc2af328e340a2678adb519e5
llesvine-joseph-poov <lesvinjoseph702@gmail.com>
2023-12-02 05:45:44 -0500

    almost_fin

--- modules/auth-service.js
+++ modules/auth-service.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-// Define User Schema
 const userSchema = new Schema({
     userName: { type: String, unique: true },
     password: String,
@@ -15,10 +15,7 @@ let User; // This will be defined on new connection
 // Initialize function to connect to the database
 function initialize() {
     return new Promise((resolve, reject) => {
-        let db = mongoose.createConnection(process.env.MONGODB, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        let db = mongoose.createConnection('mongodb+srv://llesvinejosephpoov:JxzzYv9EImesMfGS@cluster0.avvgqxq.mongodb.net/?retryWrites=true&w=majority');
 
         db.on('error', err => {
             reject(err);
@@ -31,47 +28,88 @@ function initialize() {
     });
 }
 
-function registerUser(userData) {
-    return new Promise((resolve, reject) => {
+/*function registerUser(userData) {
+    return new Promise(async (resolve, reject) => {
         if (userData.password !== userData.password2) {
             reject("Passwords do not match");
             return;
         }
 
-        let newUser = new User(userData);
+        try {
+            const saltRounds = 10;
+            const hashedPassword = bcrypt.hash(userData.password, saltRounds);
 
-        newUser.save(err => {
-            if (err) {
-                if (err.code === 11000) {
-                    reject("User Name already taken");
-                } else {
-                    reject("There was an error creating the user: " + err);
-                }
+            const newUser = new User({
+                userName: userData.userName,
+                password: hashedPassword,
+                email: userData.email,
+                loginHistory: []
+            });
+
+             newUser.save();
+
+            resolve();
+        } catch (err) {
+            if (err.code === 11000) {
+                reject("User Name already taken");
             } else {
-                resolve();
+                reject("There was an error creating the user: " + err);
             }
-        });
+        }
     });
-}
+}*/
+
+function registerUser(userData) {
+    return new Promise(function (resolve, reject) { 
+        if (userData.password == userData.password2) {
+            bcrypt.hash(userData.password, 10, function(err, hash) {
+                if (err) {
+                    reject("There was an error encrypting the password");
+                }
+                userData.password = hash;
+                let newUser = new User(userData);
+                newUser.save(function(err) {
+                    if (err) {
+                        if (err.code == 11000) {
+                            reject("User Name already taken");
+                        } else {
+                            reject("There was an error creating the user: " + err);
+                        }
+                    } else {
+                        resolve();
+                    }
+                });
+            })
+            } else {
+            reject("Passwords do not match");
+        }
+    });
+};
 
 function checkUser(userData) {
-    return new Promise((resolve, reject) => {
-        User.findOne({ userName: userData.userName }).then(user => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({ userName: userData.userName });
+
             if (!user) {
                 reject(`Unable to find user: ${userData.userName}`);
-            } else if (user.password !== userData.password) {
-                reject(`Incorrect Password for user: ${userData.userName}`);
             } else {
-                user.loginHistory.unshift({ dateTime: new Date().toString(), userAgent: userData.userAgent });
-                if (user.loginHistory.length > 8) user.loginHistory.pop();
+                const passwordMatch = await bcrypt.compare(userData.password, user.password);
 
-                User.updateOne({ userName: user.userName }, { $set: { loginHistory: user.loginHistory } })
-                    .then(() => resolve(user))
-                    .catch(err => reject(`There was an error verifying the user: ${err}`));
+                if (!passwordMatch) {
+                    reject(`Incorrect Password for user: ${userData.userName}`);
+                } else {
+                    user.loginHistory.unshift({ dateTime: new Date().toString(), userAgent: userData.userAgent });
+                    if (user.loginHistory.length > 8) user.loginHistory.pop();
+
+                    await User.updateOne({ userName: user.userName }, { $set: { loginHistory: user.loginHistory } });
+
+                    resolve(user);
+                }
             }
-        }).catch(err => {
-            reject(`Unable to find user: ${userData.userName}`);
-        });
+        } catch (err) {
+            reject(`There was an error verifying the user: ${err}`);
+        }
     });
 }
 
--- server.js
+++ server.js
@@ -13,17 +13,101 @@
 ********************************************************************************/
 
 const legoData = require("./modules/legoSets");
-const path = require('path');
-const express = require('express');
-const app = express();
 const authData = require("./modules/auth-service");
+const clientSessions = require('client-sessions');
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
 const HTTP_PORT = process.env.PORT || 8080;
+// Session configuration
+app.use(clientSessions({
+    cookieName: "session", // This is the name of the req.session object
+    secret: "Your_Secret_String", // This should be a long random string
+    duration: 2 * 60 * 1000, // Duration of the session in milliseconds (2 hours)
+    activeDuration: 60 * 1000 // The session will be extended by this many ms each request (10 minutes)
+  }));
+  
+  // Middleware to make the session available to all views
+  app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+  });
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
+
+// Helper middleware for login check
+function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+  }
+
+// GET route for login page
+app.get('/login', (req, res) => {
+    const errorMessage = req.session.errorMessage;
+
+    res.render('login',{errorMessage}); // Assuming you have a 'login.ejs' view
+});
+
+// GET route for registration page
+app.get('/register', (req, res) => {
+    let errorMessage = null;
+    let successMessage = null;
+    res.render('register',{errorMessage,successMessage}); // Assuming you have a 'register.ejs' view
+});
+
+// POST route for user registration
+app.post('/register', (req, res) => {
+    authData.registerUser(req.body)
+        .then(() => res.render('register', { successMessage: 'User created' }))
+        .catch(err => res.render('register', { errorMessage: err, userName: req.body.userName }));
+});
+/*app.get("/register", function(req, res) { 
+    res.render('register');
+  });
+  
+  app.post("/register", function(req, res) {
+    authData.registerUser(req.body)
+    .then(() => {
+        res.render('register', { successMessage: "User created!"})
+    })
+    .catch((err) => {
+        res.render('register', { errorMessag: err, userName: req.body.userName })
+    });
+  });*/
+
+// POST route for user login
+app.post('/login', (req, res) => {
+    req.body.userAgent = req.get('User-Agent');
+
+    authData.checkUser(req.body)
+        .then(user => {
+            req.session.user = {
+                userName: user.userName,
+                email: user.email,
+                loginHistory: user.loginHistory
+            };
+            res.redirect('/lego/sets');
+        })
+        .catch(err => res.render('login', { errorMessage: err, userName: req.body.userName }));
+});
+
+// GET route for logout
+app.get('/logout', (req, res) => {
+    req.session.reset();
+    res.redirect('/');
+});
+
+// GET route for user history (Protected route)
+app.get('/userHistory', ensureLogin, (req, res) => {
+    res.render('userHistory'); // Assuming you have a 'userHistory.ejs' view
+});
 
 app.get('/', (req, res) => {
     res.render("home");
